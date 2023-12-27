11092a115c12f7b0bdef8902c382318745bf1f4f
MachariaP <walburphinehas78@gmail.com>
2023-12-27 21:32:05 +0300

    a python script to convert Celsius to F ahrenheit

---
+++ prog5.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# A python program to generate a random number
+
+import random
+print(f"Random number: {random.randint(1, 100)}")
---
+++ prog7.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# A program to convert Celsius to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Conversion formula: Fahrenheit = (Celsius * 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} degree Celsius is equal to {fahrenheit} degrees Fahrenheit")
