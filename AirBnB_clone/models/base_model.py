3c19f8e5fe6c216f0cb61d70ef8ea7436c0bd298
MachariaP <walburphinehas78@gmail.com>
2024-02-09 15:49:38 +0300

    4. Create BaseModel from dictionary

--- AirBnB_clone/models/base_model.py
+++ AirBnB_clone/models/base_model.py
@@ -1,51 +1,39 @@
-#!/usr/bin/python3
-
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
-    def __init__(self):
-        """
-        Initialize a BaseModel instance with a unique ID, creation and update time
-        """
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance."""
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    if isinstance(value, str):
+                        setattr(self, key, datetime.strptime(value, time_format))
+                    else:
+                        setattr(self, key, value)
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Return a string representation of the BaseModel instance.
-        """
+        """Return the print/str representation of the BaseModel instance."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Update the public instance attribute update_at with the current datetime.
-        """
-        self.update_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Return a dictionary containing all key/values of the instance.
-        """
+        """Return a dictionary representation of the BaseModel instance."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['__created_at'] = self.created_at.isoformat()
+        obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-# Test cases
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
